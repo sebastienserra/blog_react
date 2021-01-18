d64d42c04a49317ec7c146daea80ac0a6f5115f1
sebastien <sebaserrac@yahoo.com>
2021-01-18 11:03:47 +0100

    Working on hooks and props

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
+
+
 const Home = () => {
-    return ( 
+    const[blogs, setBlogs] = useState([
+        {title: 'My new website', body:'Velit mollit ....', author: 'Mario', id:1},
+        {title:'Welcome party!', body: 'Velit mollit ....', author: 'Luigi', id:2},
+        {title:'Web dev top tips', body: 'Velit mollit ....', author: 'Mario', id:3}
+    ]);
+
+    return (
         <div className="home">
-            <h2>Home Page</h2>
+          <BlogList blogs={blogs} title="All Blogs!" />
         </div>
-     );
+    );
 }
- 
+
 export default Home;
